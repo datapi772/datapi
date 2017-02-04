211aeb9cacf3e170f09aa5f29cc90fe406700ece
Vincent Lore <vincent_lore@hotmail.com>
2017-02-04 14:29:23 -0800

    Included some content for hubble page

--- app/components/hubble/HubbleCtrl.js
+++ app/components/hubble/HubbleCtrl.js
@@ -4,4 +4,21 @@ app.controller('HubbleCtrl', function() {
     
     var vm = this;
 
+    vm.scrolltoVideo = function () {
+        vm.hideOverlay = !vm.hideOverlay;
+        var e = document.querySelector('.video-frame');
+        window.scrollTo(0, e.offsetHeight-110);
+    }
+
+    vm.slides = [
+        {
+            title: 'Hubble',
+            class: 'home-slide'
+        },
+        {
+            subtitle: '',
+            class: 'search-slide'
+        }
+    ]
+
 })
